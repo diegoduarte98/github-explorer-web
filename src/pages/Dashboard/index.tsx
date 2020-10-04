4f86bcd1c793172f3c2534de5405ebe8a0551b28
Diego Duarte <diegoduarte98@gmail.com>
2020-10-04 19:25:47 -0300

    layout do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './style';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="/">
+                    <img
+                        src="https://avatars1.githubusercontent.com/u/9434636?s=460&u=d79293dcf4852731dc51f341c84ecbc0a3825e3d&v=4"
+                        alt="Diego Duarte" />
+
+                    <div>
+                        <strong>diegoduarte98/minhas-financas-api</strong>
+                        <p>Uma aplicação simples</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 }
 
 export default Dashboard;
