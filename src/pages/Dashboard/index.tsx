2a2c2204a9f6b43bdfc1a36c98c8e4fba7519086
Diego Duarte <diegoduarte98@gmail.com>
2020-10-11 17:44:36 -0300

    navegacao entre paginas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, FormEvent } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories, Error } from './style';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 interface Repository {
@@ -68,7 +69,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="/">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login} />
@@ -79,7 +80,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
