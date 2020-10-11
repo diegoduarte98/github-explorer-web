0b867af3acd7391f43470f3435a2471418e9e0c4
Diego Duarte <diegoduarte98@gmail.com>
2020-10-11 18:46:47 -0300

    finalizando

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories, Error } from './style';
+import { Title, Form, Repositories, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
